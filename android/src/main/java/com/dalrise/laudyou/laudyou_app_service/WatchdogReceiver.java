eb7863b3e1daa6050edd140e2ca3994f52317e4e
mac <hjo@dalrise.co.kr>
2022-02-16 20:25:49 +0900

    fix: add service2

--- android/src/main/java/com/dalrise/laudyou/laudyou_app_service/WatchdogReceiver.java
+++ android/src/main/java/com/dalrise/laudyou/laudyou_app_service/WatchdogReceiver.java
@@ -1,4 +1,20 @@
 package com.dalrise.laudyou.laudyou_app_service;
 
-public class WatchdogReceiver {
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+import androidx.core.content.ContextCompat;
+
+public class WatchdogReceiver extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        if(!BackgroundService.isManuallyStopped(context)){
+            if (BackgroundService.isForegroundService(context)){
+                ContextCompat.startForegroundService(context, new Intent(context, BackgroundService.class));
+            } else {
+                context.startService(new Intent(context, BackgroundService.class));
+            }
+        }
+    }
 }
