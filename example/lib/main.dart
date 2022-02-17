3da45009659413e9d64f8729a9a0b19ae338f92a
JO <johyun0218@naver.com>
2022-02-17 15:05:52 +0900

    fix: test service

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,11 +1,93 @@
+import 'package:device_info_plus/device_info_plus.dart';
 import 'package:flutter/material.dart';
 import 'dart:async';
-
+import 'dart:io';
 import 'package:flutter/services.dart';
 import 'package:laudyou_app_service/laudyou_app_service.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await initializeService();
   runApp(const MyApp());
+}
+
+Future<void> initializeService() async {
+  final service = LaudyouAppService();
+  await service.configure(
+    androidConfiguration: AndroidConfiguration(
+      // this will executed when app is in foreground or background in separated isolate
+      onStart: onStart,
+
+      // auto start service
+      autoStart: true,
+      isForegroundMode: true,
+    ),
+    iosConfiguration: IosConfiguration(
+      // auto start service
+      autoStart: true,
+
+      // this will executed when app is in foreground in separated isolate
+      onForeground: onStart,
+
+      // you have to enable background fetch capability on xcode project
+      onBackground: onIosBackground,
+    ),
+  );
+}
+
+void onIosBackground() {
+  WidgetsFlutterBinding.ensureInitialized();
+  print('FLUTTER BACKGROUND FETCH');
+}
+
+void onStart() {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  final service = LaudyouAppService();
+  service.onDataReceived.listen((event) {
+    if (event!["action"] == "setAsForeground") {
+      service.setForegroundMode(true);
+      return;
+    }
+
+    if (event["action"] == "setAsBackground") {
+      service.setForegroundMode(false);
+    }
+
+    if (event["action"] == "stopService") {
+      service.stopBackgroundService();
+    }
+  });
+
+  // bring to foreground
+  service.setForegroundMode(true);
+  Timer.periodic(const Duration(seconds: 1), (timer) async {
+    if (!(await service.isServiceRunning())) timer.cancel();
+    service.setNotificationInfo(
+      title: "LaudYou App Service",
+      content: "Updated at ${DateTime.now()}",
+    );
+
+    // test using external plugin
+    final deviceInfo = DeviceInfoPlugin();
+    String? device;
+    if (Platform.isAndroid) {
+      final androidInfo = await deviceInfo.androidInfo;
+      device = androidInfo.model;
+    }
+
+    if (Platform.isIOS) {
+      final iosInfo = await deviceInfo.iosInfo;
+      device = iosInfo.model;
+    }
+
+    service.sendData(
+      {
+        "current_date": DateTime.now().toIso8601String(),
+        "device": device,
+      },
+    );
+  });
 }
 
 class MyApp extends StatefulWidget {
@@ -17,7 +99,7 @@ class MyApp extends StatefulWidget {
 
 class _MyAppState extends State<MyApp> {
   String _platformVersion = 'Unknown';
-
+  String text = "Stop Service";
   @override
   void initState() {
     super.initState();
@@ -51,10 +133,67 @@ class _MyAppState extends State<MyApp> {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: const Text('Plugin example app'),
+          title: const Text('Plugin Laudyou App Service sample app'),
         ),
         body: Center(
-          child: Text('Running on: $_platformVersion\n'),
+          child: Column(
+            children: [
+              Text('Running on: $_platformVersion\n'),
+              StreamBuilder<Map<String, dynamic>?>(
+                stream: LaudyouAppService().onDataReceived,
+                builder: (context, snapshot) {
+                  if (!snapshot.hasData) {
+                    return const Center(
+                      child: CircularProgressIndicator(),
+                    );
+                  }
+
+                  final data = snapshot.data!;
+                  String? device = data["device"];
+                  DateTime? date = DateTime.tryParse(data["current_date"]);
+                  return Column(
+                    children: [
+                      Text(device ?? 'Unknown'),
+                      Text(date.toString()),
+                    ],
+                  );
+                },
+              ),
+              ElevatedButton(
+                child: const Text("Foreground Mode"),
+                onPressed: () {
+                  LaudyouAppService().sendData({"action": "setAsForeground"});
+                },
+              ),
+              ElevatedButton(
+                child: const Text("Background Mode"),
+                onPressed: () {
+                  LaudyouAppService().sendData({"action": "setAsBackground"});
+                },
+              ),
+              ElevatedButton(
+                child: Text(text),
+                onPressed: () async {
+                  final service = LaudyouAppService();
+                  var isRunning = await service.isServiceRunning();
+                  if (isRunning) {
+                    service.sendData(
+                      {"action": "stopService"},
+                    );
+                  } else {
+                    service.start();
+                  }
+
+                  if (!isRunning) {
+                    text = 'Stop Service';
+                  } else {
+                    text = 'Start Service';
+                  }
+                  setState(() {});
+                },
+              ),
+            ],
+          ),
         ),
       ),
     );
